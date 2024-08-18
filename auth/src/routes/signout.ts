b82e261c1470f0be4dfeb4589ec3a5eb78f2d394
dodyvirgiawan <dody.virgiawan97@gmail.com>
2024-08-18 17:08:05 +0700

    add more auth routes & middlewares

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,12 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/api/users/signout', (req, res) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signout',
+  (req, res) => {
+    req.session = null; // from cookie-session library docs
+    res.send({});
+  }
+);
 
 export { router as signoutRouter };
